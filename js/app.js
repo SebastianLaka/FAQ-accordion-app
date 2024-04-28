1cc070fff0c581a074e453a06ce387073ac6991a
Sebastian <sebastianlakla@gmail.com>
2024-04-28 19:49:27 +0200

    Create icon plus variable

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 "use strict";
 
 const accordionItem = document.querySelectorAll(".accordion-question")
-const accordionContent = document.querySelectorAll(".accordion-container__conten")
+const accordionContent = document.querySelectorAll(".accordion-container__content")
+const plusIcon = document.querySelectorAll('.accordion-question__plus-icon')
