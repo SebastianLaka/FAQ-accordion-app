5aa2bd9fe77c9a2cab28fa3069f1cb22d1133c9b
Sebastian <sebastianlakla@gmail.com>
2024-04-29 20:11:40 +0200

    Add listener at accordion item

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 "use strict";
 
-const accordionItem = document.querySelectorAll(".accordion-question")
-const accordionContent = document.querySelectorAll(".accordion-container__content")
-const plusIcon = document.querySelectorAll('.accordion-question__plus-icon')
+const accordionItem = document.querySelectorAll(".accordion-question");
+const accordionContent = document.querySelectorAll(
+  ".accordion-container__content"
+);
+const plusIcon = document.querySelectorAll(".accordion-question__plus-icon");
+const plusMinus = document.querySelectorAll(".accordion-question__minus-icon");
+
+accordionItem.forEach((accItem) => {
+  accItem.addEventListener("click", () => {
+    
+  });
+});
