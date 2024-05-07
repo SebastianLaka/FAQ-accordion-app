e29703d9b9d206fe2bc3c343de4685828521148a
Sebastian <sebastianlakla@gmail.com>
2024-05-07 20:35:11 +0200

    Delete variables

--- js/app.js
+++ js/app.js
@@ -1,13 +1,11 @@
 "use strict";
 const accordionItem = document.querySelectorAll(".accordion-question");
-const accordionContent = document.querySelector(".accordion-container__content");
-const minusIcon = document.querySelector(".accordion-question__minus-icon");
 
 accordionItem.forEach((accItem) => {
   accItem.addEventListener("click", (e) => {
    const target = e.currentTarget;
    console.log(target);
-   if(target.classList.contains( 'accordion-question')){
+   if(target.classList.contains('accordion-question')){
     target.nextElementSibling.classList.toggle('show-answer');
    }
   });
