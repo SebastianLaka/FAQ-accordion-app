ae99bf3500d9ea2088163048e15b3d53d62d2eb6
Sebastian <sebastianlakla@gmail.com>
2024-05-07 20:13:07 +0200

    Add function for show answers in accordion, migrate classes in scss

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 "use strict";
 const accordionItem = document.querySelectorAll(".accordion-question");
 const accordionContent = document.querySelector(".accordion-container__content");
+const minusIcon = document.querySelector(".accordion-question__minus-icon");
 
 accordionItem.forEach((accItem) => {
   accItem.addEventListener("click", (e) => {
-   const target = e.currentTarget
+   const target = e.currentTarget;
    console.log(target);
-    // const minusIcon = document.querySelector(".accordion-question__minus-icon");
-    // const plusIcon = document.querySelector(".accordion-question__plus-icon");
-    accordionContent.classList.toggle('show-answer')
-   
-  }, true);
+   if(target.classList.contains( 'accordion-question')){
+    target.nextElementSibling.classList.toggle('show-answer');
+   }
+  });
 });
+
